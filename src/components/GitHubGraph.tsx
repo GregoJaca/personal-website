9d254da5270c8c525c4c0338595e2c6549c1c3c4
GregoJaca <grego.repollo@gmail.com>
2026-04-15 23:06:21 +0200

    prettify the glass block

--- src/components/GitHubGraph.tsx
+++ src/components/GitHubGraph.tsx
@@ -9,11 +9,11 @@ const GitHubGraph = () => {
     const theme = {
         light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
         dark: [
-            'rgba(255, 255, 255, 0.05)', 
-            '#0e4429',                   
-            '#006d32',                   
-            '#26a641',                   
-            '#39d353',                   
+            'rgba(255, 255, 255, 0.05)',
+            '#0e4429',
+            '#006d32',
+            '#26a641',
+            '#39d353',
         ],
     };
 
@@ -22,9 +22,9 @@ const GitHubGraph = () => {
             <h2 className="text-xs font-medium text-zinc-500 mb-6 uppercase tracking-widest">
                 Contributions in the last year{totalContributions !== null ? `: ${totalContributions}` : ''}
             </h2>
-            <div id="no-scrollbar-container" className="bg-zinc-900/40 p-10 rounded-xl border border-zinc-800/10 backdrop-blur-md overflow-x-auto transition-all duration-500">
-                <GitHubCalendar 
-                    username="GregoJaca" 
+            <div id="no-scrollbar-container" className="bg-zinc-900/5 p-10 rounded-2xl border border-white/10 backdrop-blur shadow-2xl overflow-x-auto transition-all duration-500">
+                <GitHubCalendar
+                    username="GregoJaca"
                     colorScheme='dark'
                     theme={theme}
                     hideTotalCount={true}
